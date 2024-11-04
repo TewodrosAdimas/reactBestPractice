1da4bec6a77d8ac29c6792772e700f19ed986412
TewodrosAdimas <yosefenadagim@gmail.com>
2024-11-04 15:35:36 +0300

    conditional rendering

--- counter/src/components/counter.jsx
+++ counter/src/components/counter.jsx
@@ -4,7 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Counter extends Component {
     state= {
-        count: 0
+        count: 10,
+        tags : ['tag1', 'tag2', 'tag3']
     };
 
     styles = {
@@ -13,18 +14,26 @@ class Counter extends Component {
     };
     
 
+    renderTags(){
+        if(this.state.tags.length === 0) return <p>No Tags!</p>;
+        return <ul>{ this.state.tags.map(tag=><li key={tag}>{ tag }</li>)}</ul>   
+    }
+
     render() { 
 
         let classes = "badge m-2 bg-";
         classes += (this.state.count === 0) ? "warning" : "primary";
 
         return (
-            <React.Fragment>
-                <span style= {this.styles } className={classes}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                </React.Fragment>);
-    }
+                <div>
+                    <span style= {this.styles } className={classes}>{this.formatCount()}</span>
+                    <button className="btn btn-secondary btn-sm">Increment</button> 
+                    {this.renderTags()}
+                </div>
 
+
+        );
+    }
 
     formatCount() {
         const {count} = this.state;
