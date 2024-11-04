3843463500c4d6c8a30eee532121ee9449fa8bfc
TewodrosAdimas <yosefenadagim@gmail.com>
2024-11-04 14:53:52 +0300

    dynamic rendering

--- counter/src/components/counter.jsx
+++ counter/src/components/counter.jsx
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class Counter extends Component {
+    state= {
+        count: 0
+    };
+
+    styles = {
+        fontSize: 30,
+        fontWeight: 'bold'
+    };
+    
+
     render() { 
+
+        let classes = "badge m-2 bg-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+
         return (
             <React.Fragment>
-                <h1>Hello React</h1>
-                <button>Increnment</button>
+                <span style= {this.styles } className={classes}>{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
                 </React.Fragment>);
+    }
+
+
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
 
