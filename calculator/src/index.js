2edb5c4a647128e59b370558f2dbf3985d316f68
TewodrosAdimas <yosefenadagim@gmail.com>
2024-11-05 12:45:38 +0300

    calculator visual done

---
+++ calculator/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+function Calculator() {
+  return (
+    <div className='calculator'>
+      <div className="display">0</div>
+      <CalcButton value = "7"/>
+      <CalcButton value = "8"/>
+      <CalcButton value = "9"/>
+      <CalcButton className = "operator"  value = "/"/>
+
+      <CalcButton value = "4"/>
+      <CalcButton value = "5"/>
+      <CalcButton value = "6"/>
+      <CalcButton className = "operator" value = "*"/>
+
+      <CalcButton value = "1"/>
+      <CalcButton value = "2"/>
+      <CalcButton value = "3"/>
+      <CalcButton className = "operator" value = "-"/>
+
+      <CalcButton value = "C"/>
+      <CalcButton value = "0"/>
+      <CalcButton value = "="/>
+      <CalcButton className = "operator" value = "+"/>
+
+    </div>
+  )
+}
+
+function CalcButton(props){
+  return (
+    <button className= {props.className} >{props.value}</button>
+  )
+}
+
+ReactDOM.render(<div className='container'><Calculator/></div>, document.getElementById('root'));
